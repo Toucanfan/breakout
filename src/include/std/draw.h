b2c3fcbcc71aa74d72d433bbe7f9eaff5a5c10b1
Troels Folke <troelsmf@gmail.com>
2014-06-12 14:17:16 +0200

    initial drawing application layer (std_draw)

---
+++ src/include/std/draw.h
@@ -0,0 +1,35 @@
+#ifndef STD_DRAW_H
+#define STD_DRAW_H
+
+/* data structures */
+struct std_draw_point {
+	int x;
+	int y;
+};
+
+struct std_draw_window_style {
+	char hori;
+	char vert;
+	char tr;
+	char tl;
+	char bl;
+	char hstart;
+	char hend;
+	char fill;
+};
+
+struct std_draw_window {
+	struct std_draw_point tl;
+	struct std_draw_point br;
+	struct std_draw_point cursor;
+	struct std_draw_window_style style;
+	char title[16];
+};
+
+
+
+/* function prototypes */
+extern void std_draw_window(struct std_draw_window *window);
+
+
+#endif /* std/draw.h */
---
+++ src/std/draw.c
@@ -0,0 +1,57 @@
+#include "std/draw.h"
+#include "std/tty.h"
+
+void std_draw_window(struct std_draw_window *window)
+{
+	int i, j;
+	int textLength = 0;
+	
+	int width = window->br.x - window->tl.x + 1;
+	int height = window->br.y - window->tl.y + 1;
+
+	int headerExtra;
+
+ 	for(i = 0; title[i] != '\0'; i++) {
+		textLength++;
+	}
+
+	headerExtra = width - 8 - textLength;
+
+	std_tty_gotoxy(window->tl.x, window->tl.y);
+	printf("%c", window->style.tl);
+	printf("%c", window->style.hstart);
+	std_tty_reverse(1);
+	std_tty_printf("  %s  ", window->title);
+	if(window->style.fill) {
+		for(i = 0; i < headerExtra; i++) {
+			std_tty_printf(" ");
+		}
+	}
+	reverse(0);
+	std_tty_printf("%c", window->style.hend);
+	if(!window->style.fill) {
+		for(i = 0; i < headerExtra; i++) {
+			std_tty_printf("%c", window->style.hori);
+		}
+	}
+	std_tty_printf("%c\n", window->style.tr);
+	
+	for(i = 1; i < height; i++) {
+		std_tty_gotoxy(window->tl.x, window->tl.y + i);
+		std_tty_printf("%c", window->style.vert);
+		for(j = 0; j < width - 2; j++) {
+			std_tty_printf(" ");
+		}
+		std_tty_printf("%c", window->style.vert);
+	}
+	
+	std_tty_gotoxy(window->tl.x, window->br.y);
+	std_tty_printf("%c", window->style.bl);
+	for(i = 0; i < width - 2; i++) {
+		std_tty_printf("%c", window->style.hori);
+	}
+	std_tty_printf("%c", window->style.br);
+
+	window->cursor.x = window->tl.x + 1;
+	window->cursor.y = window->tl.y + 1;
+	
