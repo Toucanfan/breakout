225c1a508f60bd506e1d8a7b0d85afb000a9f661
Troels Folke <troelsmf@gmail.com>
2014-06-13 13:24:55 +0200

    fix related to hal_button B3

--- src/hal/hal_button.c
+++ src/hal/hal_button.c
@@ -7,7 +7,7 @@ char hal_button_pressed(void)
 		return HAL_BUTTON_B1;
 	else if (~PFIN & 0x40) //PF6
 		return HAL_BUTTON_B2;
-	else if (~PDIN & 0x80) //PF7
+	else if (~PFIN & 0x80) //PF7
 		return HAL_BUTTON_B3;
 	else
 		return HAL_BUTTON_NONE;
