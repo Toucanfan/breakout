f53988acbaca8166571441e11313a49f5d876f36
Heyer <danielsafari@yahoo.dk>
2014-06-16 14:53:49 +0200

    potential block_draw fix

--- src/app/blocks.c
+++ src/app/blocks.c
@@ -4,10 +4,10 @@
 
 #define BLOCK_ROWS 3
 #define BLOCK_PADDING_TOP 4
-#define BLOCK_PER_ROW 32 //size of long
-#define BLOCK_SIZE 2
-#define BLOCK_INTERDIST_X 2
-#define BLOCK_INTERDIST_Y 2
+#define BLOCK_COLUMNS 32 //size of long
+#define BLOCK_LENGTH 2
+#define BLOCK_INTERDIST_X 1
+#define BLOCK_INTERDIST_Y 1
 
 #define WIDTH 170
 #define HEIGHT 60
@@ -19,14 +19,16 @@ void app_map_draw_blocks(void) {
 	int k;
 	struct std_draw_box cur_block;
 	
-	cur_block.tl.x = WIDTH / 2 - BLOCK_PER_ROW * (BLOCK_SIZE+BLOCK_INTERDIST_X) / 2;
+	int block_start_x = WIDTH / 2 - BLOCK_COLUMNS * (BLOCK_LENGTH+BLOCK_INTERDIST_X) / 2;
+	
+	cur_block.tl.x = block_start_x
 	cur_block.tl.y = BLOCK_PADDING_TOP;
-	cur_block.br.x = cur_block.tl.x + BLOCK_SIZE;
+	cur_block.br.x = cur_block.tl.x + (BLOCK_LENGTH - 1);
 	cur_block.br.y = cur_block.tl.y;
 	cur_block.color = STD_TTY_BCOLOR_GRAY;
 	
 	for (i = 0; i < BLOCK_ROWS; i++) {
-		for (j = 0; j < BLOCK_PER_ROW; j++) {
+		for (j = 0; j < BLOCK_COLUMNS; j++) {
 			if(blocks[i] & (0x80000000 >> j)) {
 				std_draw_box(&cur_block);
 				for (k = 1; k <= BLOCK_INTERDIST_X; k++) {
@@ -34,18 +36,18 @@ void app_map_draw_blocks(void) {
 					std_tty_printf(" "); // blank box
 				}
 			} else {
-				for (k = 0; k < BLOCK_INTERDIST_X + BLOCK_SIZE; k++) {
+				for (k = 0; k <= BLOCK_INTERDIST_X + BLOCK_LENGTH; k++) {
 					std_tty_gotoxy(cur_block.tl.x+k,cur_block.tl.y);
 					std_tty_printf(" "); // blank box
 				}
 			}
-			cur_block.tl.x += BLOCK_INTERDIST_X + BLOCK_SIZE;
-			cur_block.br.x += BLOCK_INTERDIST_X + BLOCK_SIZE;
+			cur_block.tl.x += BLOCK_INTERDIST_X + BLOCK_LENGTH;
+			cur_block.br.x += BLOCK_INTERDIST_X + BLOCK_LENGTH;
 		}	
-		cur_block.tl.y += BLOCK_INTERDIST_Y;
-		cur_block.br.y += BLOCK_INTERDIST_Y;
+		cur_block.tl.x = block_start_x;
+		cur_block.br.x = cur_block.tl.x + (BLOCK_LENGTH-1);
+		cur_block.tl.y += (BLOCK_INTERDIST_Y + 1);
+		cur_block.br.y += (BLOCK_INTERDIST_Y + 1);
 		
-		cur_block.tl.x = WIDTH / 2 - BLOCK_PER_ROW * (BLOCK_SIZE+BLOCK_INTERDIST_X) / 2;
-		cur_block.br.x = cur_block.tl.x + BLOCK_INTERDIST_X;
 	}		
 }
