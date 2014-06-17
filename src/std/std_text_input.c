58510a75a0db4c3ec5655b48defd9af2f55c3e71
Troels Folke <troelsmf@gmail.com>
2014-06-17 12:36:17 +0200

    Update cursor position before fetching key

--- src/std/std_text_input.c
+++ src/std/std_text_input.c
@@ -11,6 +11,7 @@ void std_text_input_create(struct std_draw_point *point, char *str, char str_len
 	str[str_length - 1] = '\0';
 
 	while(input_loop){
+		std_tty_gotoxy(point->x + i, point->y);
 		key = getch();
 		if(97 <= key && key <= 122 && i < 3) {
 			str[i] = key;
@@ -25,7 +26,6 @@ void std_text_input_create(struct std_draw_point *point, char *str, char str_len
 
 		std_tty_gotoxy(point->x, point->y);
 		std_tty_printf("\n%s", str);
-		std_tty_gotoxy(point->x + i, point->y);
 	}
 
 	for(i++; i < (str_length - 1); i++)
