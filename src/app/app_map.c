14b454993038908067fe6ec51c04d5f317600458
Troels Folke <troelsmf@gmail.com>
2014-06-17 15:42:07 +0200

    app_map: use timer and check paddle bounds

--- src/app/app_map.c
+++ src/app/app_map.c
@@ -7,12 +7,13 @@
 extern long *draw_blocks(void);
 extern char test_block_collision(struct ball *ball, long *block_locations);
 
-#define WIDTH 170
-#define HEIGHT 60
-#define RBORDER std_fixpt_i2f(WIDTH-4)
-#define BBORDER std_fixpt_i2f(HEIGHT-2)
-#define TBORDER std_fixpt_i2f(3)
-#define LBORDER std_fixpt_i2f(4)
+#define WIDTH 120
+#define HEIGHT 30
+#define RBORDER std_fixpt_i2f(WIDTH-2)
+#define BBORDER std_fixpt_i2f(HEIGHT-1)
+#define TBORDER std_fixpt_i2f(1)
+#define LBORDER std_fixpt_i2f(2)
+#define FIXPT_1 std_fixpt_i2f(1)
 
 #define COLLISION_NONE 0x0
 #define COLLISION_HORIZONTAL 0x1
@@ -24,7 +25,7 @@ static struct ball {
 };
 
 static struct paddle {
-	long x_left;
+	long x;
 	long vel;
 };
 
@@ -33,6 +34,7 @@ static void draw_borders(void)
 	struct std_draw_box topb;
 	struct std_draw_box leftb;
 	struct std_draw_box rightb;
+	struct std_draw_box botb;
 
 	topb.tl.x = 1;
 	topb.tl.y = 1;
@@ -52,9 +54,16 @@ static void draw_borders(void)
 	rightb.br.y = HEIGHT;
 	rightb.color = STD_TTY_BCOLOR_GRAY;
 
+	botb.tl.x = 1;
+	botb.tl.y = HEIGHT;
+	botb.br.x = WIDTH;
+	botb.br.y = HEIGHT;
+	botb.color = STD_TTY_BCOLOR_GRAY;
+
 	std_draw_box(&leftb);
 	std_draw_box(&topb);
 	std_draw_box(&rightb);
+	//std_draw_box(&botb);
 }
 
 static void draw_ball(struct ball *ball)
@@ -68,11 +77,10 @@ static void draw_ball(struct ball *ball)
 
 	if (x != x_old && y != y_old) {
 		/* remove drawing of last ball */
-		/*
+		
 		std_tty_gotoxy(x_old, y_old);
 		std_tty_set_fcolor(STD_TTY_FCOLOR_BLACK);
 		std_tty_printf("o");
-		*/
 	
 		/* draw ball at new position */
 		std_tty_gotoxy(x, y);
@@ -86,25 +94,28 @@ static void draw_ball(struct ball *ball)
 
 static void draw_paddle(struct paddle *paddle)
 {
-	static int x_old = 0;
+	static int x_old = 10;
 	int x;
-	struct std_draw_box box;
+	static struct std_draw_box box;
 
-	x = std_fixpt_f2i(paddle->x_left);
+	x = std_fixpt_f2i(paddle->x);
+
+	if (x-3 < 0 || x+3 > WIDTH) {
+		paddle->x = std_fixpt_i2f(x_old);
+		return;
+	}
 
 	if (x != x_old) {
 		/* remove drawing of last paddle */
-		box.color = STD_TTY_BCOLOR_BLACK;
-		std_draw_box(&box);
+		std_tty_gotoxy(x_old-3, HEIGHT);
+		std_tty_set_bcolor(STD_TTY_BCOLOR_BLACK);
+		std_tty_printf("       ");
 	
 		/* draw paddle at new position */
-		box.tl.x = x;
-		box.tl.y = HEIGHT;
-		box.br.x = x+7;
-		box.br.y = HEIGHT;
-		box.color = STD_TTY_BCOLOR_GRAY;
-		std_draw_box(&box);
-
+		std_tty_gotoxy(x-3, HEIGHT);
+		std_tty_set_bcolor(STD_TTY_BCOLOR_GRAY);
+		std_tty_printf("       ");
+		std_tty_set_bcolor(STD_TTY_BCOLOR_BLACK);
 		x_old = x;
 	}
 }
@@ -113,19 +124,19 @@ static char test_collision(struct ball *ball, long *block_locations)
 {
 	char r;
 
-	if (ball->pos.x <= LBORDER)
+	if (ball->pos.x + ball->vel.x <= LBORDER)
 		return COLLISION_VERTICAL;
 
-	else if (ball->pos.x >= RBORDER)
+	else if (ball->pos.x + ball->vel.x >= RBORDER)
 		return COLLISION_VERTICAL;
 
 	else if (r = test_block_collision(ball, block_locations))
 		return r;
 
-	else if (ball->pos.y <= TBORDER)
+	else if (ball->pos.y + ball->vel.y <= TBORDER)
 		return COLLISION_HORIZONTAL;
 
-	else if (ball->pos.y >= BBORDER)
+	else if (ball->pos.y + ball->vel.y >= BBORDER)
 		return COLLISION_HORIZONTAL;
 
 	else 
@@ -142,7 +153,6 @@ static void handle_collision(struct ball *ball, char coll_type)
 		ball->vel.y = -(ball->vel.y);
 		break;
 	default:
-	case COLLISION_NONE:
 		break;
 	}
 }
@@ -156,37 +166,42 @@ void app_map_refresh(char should_reset)
 	if (should_reset) {
 		ball.pos.x = std_fixpt_i2f(WIDTH/2);
 		ball.pos.y = std_fixpt_i2f(HEIGHT/2);
-		ball.vel.x = std_fixpt_div(std_fixpt_i2f(-1), std_fixpt_i2f(64));
-		ball.vel.y = std_fixpt_div(std_fixpt_i2f(-1), std_fixpt_i2f(64));
-		paddle.x_left = std_fixpt_i2f(WIDTH/2-3);
-		paddle.vel = std_fixpt_i2f(1)/16;
+		ball.vel.x = std_fixpt_div(std_fixpt_i2f(-1), std_fixpt_i2f(2));
+		ball.vel.y = std_fixpt_div(std_fixpt_i2f(-1), std_fixpt_i2f(2));
+		paddle.x = std_fixpt_i2f(WIDTH/2);
+		paddle.vel = std_fixpt_i2f(1)/1;
 		std_tty_clrscr();
 		draw_borders();
 		block_locations = draw_blocks();
 		return;
 	}
-	collision = test_collision(&ball, block_locations);
-	handle_collision(&ball, collision);
+	if (collision = test_collision(&ball, block_locations))
+		handle_collision(&ball, collision);
 	ball.pos.x += ball.vel.x;
 	ball.pos.y += ball.vel.y;
-	draw_paddle(&paddle);
 	draw_ball(&ball);
-	if (std_button_pressed(STD_BUTTON_LEFT))
-		paddle.x_left -= paddle.vel;
-	else if (std_button_pressed(STD_BUTTON_RIGHT))
-		paddle.x_left += paddle.vel;
-
-
+	switch (std_button_pressed()) {
+		case STD_BUTTON_LEFT:
+			paddle.x = (paddle.x == 0+3) ? paddle.x : paddle.x - paddle.vel;
+			break;
+		case STD_BUTTON_RIGHT:
+			paddle.x += paddle.vel;
+			break;
+		default:
+			break;
+	}
+	draw_paddle(&paddle);
 }
 
 void main(void)
 {
 	std_tty_init();
 	std_tty_clrscr();
-	//std_timer_init();
+	std_tty_gotoxy(1,1);
+	std_timer_init();
 	app_map_refresh(1);
 	while (1) {
-	//	if (std_timer_read(STD_TIMER_0))
+		if (std_timer_read(STD_TIMER_0))
 			app_map_refresh(0);
 	}
 
