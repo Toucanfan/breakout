ace0be68821afad2621d972efc26a2f9be5e1aba
Troels Folke <troelsmf@gmail.com>
2014-06-20 13:12:01 +0200

    Create std/led.h

---
+++ src/include/std/led.h
@@ -0,0 +1,11 @@
+#ifndef STD_LED_H
+#define STD_LED_H
+
+#include "hal/led.h"
+
+#define std_led_init hal_led_init
+#define std_led_set_string hal_led_set_string
+#define std_led_set_mode hal_led_set_mode
+#define std_led_refresh hal_led_refresh
+
+#endif /* STD_LED_H */
