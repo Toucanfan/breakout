04270462f2f03881b6b6a21330106b5db40259cb
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2014-06-20 14:27:06 +0200

    Middle button is ESC for text input

--- src/std/std_kb.c
+++ src/std/std_kb.c
@@ -1,10 +1,14 @@
 #include <uart.h>
 #include <ez8.h>
 #include "std/kb.h"
+#include "hal/button.h"
 
 char get_char()
 {
-	while(!kbhit()){};
+	while(!kbhit() && hal_button_pressed() != HAL_BUTTON_B2){};
+
+	if(hal_button_pressed() == HAL_BUTTON_B2)
+		return ESC7;
 
 	return U0RXD;
 }
