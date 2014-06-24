e52b72f638d3105408283bfec2c857b15421ec8c
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2014-06-24 10:56:53 +0200

    Docs for std_tty

--- src/std/std_tty.c
+++ src/std/std_tty.c
@@ -1,16 +1,29 @@
+/**
+ * @file
+ * @version 1.0
+ * @section std_tty-description Description
+ *
+ * Functions for controlling ANSI printing for serial terminal
+ */
+
 #include "std/tty.h"
 
 #define ESC "\x1B"
 
-
+//! Clear to end of line
 void std_tty_clreol(void) {
 	std_tty_printf(ESC"[K");
 }
 
+//! Move cursor to (x, y)
 void std_tty_gotoxy(int x, int y) {
 	std_tty_printf(ESC"[%d;%dH", y, x);
 }
 
+//! Set underline style
+/**
+ * @param on 1 for underline on, 0 for off
+ */
 void std_tty_underline(char on) {
 	if (on)
 		std_tty_printf(ESC"[4m");
@@ -18,6 +31,10 @@ void std_tty_underline(char on) {
 		std_tty_printf(ESC"[24m");
 }
 
+//! Set reversed color style
+/**
+ * @param on 1 for on, 0 for off
+ */
 void std_tty_reverse(char on) {
 	if (on) {
 		std_tty_printf(ESC"[7m");
@@ -26,6 +43,10 @@ void std_tty_reverse(char on) {
 	}
 }
 
+//! Set blink style
+/**
+ * @param on 1 for on, 0 for off
+ */
 void std_tty_blink(char on) {
 	if (on) {
 		std_tty_printf(ESC"[5m");
@@ -34,6 +55,10 @@ void std_tty_blink(char on) {
 	}
 }
 
+//! Set foreground color
+/**
+ * @param fcolor The color
+ */
 void std_tty_set_fcolor(char fcolor)
 {
 	int type = 22;
@@ -44,11 +69,16 @@ void std_tty_set_fcolor(char fcolor)
 	std_tty_printf(ESC"[%d;%dm", type, fcolor+30);
 }
 
+//! Set background color
+/**
+ * @param bcolor The color
+ */
 void std_tty_set_bcolor(char bcolor)
 {
 	std_tty_printf(ESC"[%dm", bcolor+40);
 }
 
+//! Clear screen
 void std_tty_clrscr(void) {
 	std_tty_set_bcolor(STD_TTY_BCOLOR_BLACK);
 	std_tty_printf(ESC"[2J"ESC"[1;1H");
